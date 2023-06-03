2234dbe7513d6fa3e779f35d4b1aa79800cef3d2
Loïc Riegel <96702577+LoicRiegel@users.noreply.github.com>
2023-06-03 23:38:58 +0200

    Improve dep inversion

--- sw-design/presentation_snippets/255_dependency_inversion.py
+++ sw-design/presentation_snippets/255_dependency_inversion.py
@@ -18,16 +18,15 @@ def main():
     process_payment(5000, stripe_payment_handler)
 
 # After
-class StripePaymentHandler:
-    ...
-    def handle_payment(self, total: int):
-        ...
-
 class PaymentHandler(ABC):
     @abstractmethod
     def handle_payment(self, total: int) -> None:
         """Handle payment."""
 
+class StripePaymentHandler:
+    ...
+    def handle_payment(self, total: int):
+        ...
 
 def process_payment(price: int, payment_handler: PaymentHandler) -> None:
     logging.info(f"Order total is ${total/100:.2f}.")
@@ -39,3 +38,73 @@ def main():
     stripe_payment_handler = StripePaymentHandler()
     process_payment(5000, stripe_payment_handler)
 
+
+# After part 2
+class PaymentHandler(ABC):
+    @abstractmethod
+    def handle_payment(self, total: int) -> None:
+        """Handle payment."""
+
+class StripePaymentHandler:
+    ...
+    def handle_payment(self, total: int):
+        ...
+
+class PayPalPaymentHandler:
+    ...
+    def handle_payment(self, total: int):
+        ...
+
+
+def process_payment(price: int, payment_handler: PaymentHandler) -> None:
+    logging.info(f"Order total is ${total/100:.2f}.")
+    payment_handler.handle_payment(total)
+    logging.info("Order completed.")
+
+def main():
+    ...
+    stripe_payment_handler = StripePaymentHandler()()
+    process_payment(5000, stripe_payment_handler)
+
+
+# After part 2
+class PaymentHandler(ABC):
+    @abstractmethod
+    def handle_payment(self, total: int) -> None:
+        """Handle payment."""
+
+class StripePaymentHandler:
+    ...
+    def handle_payment(self, total: int):
+        ...
+
+class PayPalPaymentHandler:
+    ...
+    def handle_payment(self, total: int):
+        ...
+
+
+def process_payment(price: int, payment_handler: PaymentHandler) -> None:
+    logging.info(f"Order total is ${total/100:.2f}.")
+    payment_handler.handle_payment(total)
+    logging.info("Order completed.")
+
+def main():
+    ...
+    paypal_payment_handler = PayPalPaymentHandler()()
+    process_payment(5000, paypal_payment_handler)
+
+
+"""
+Initial: where we left of after dependency injection.
+process_payment is still depending of StripePaymentHandler specifically. So it can only work with it.
+We can only use Stripe.
+
+Step 1: create an abstration and use it in process_payment
+
+Step 2: now we can have as many payment handlers as we want. As soon as they implement handle_payment
+as specified in our interface, process_payment can use it.
+For instance, create a PayPalPaymentHandler if users want to use PayPal now.
+
+Step 3: in the main, it's as easy as changing the resources we create and supplying a different object.
+"""
