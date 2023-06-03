ce21396236d5956b78d7d53a4dfc5667e02cf503
Loïc Riegel <96702577+LoicRiegel@users.noreply.github.com>
2023-06-03 23:29:47 +0200

    Dependency injection bad examples

---
+++ sw-design/presentation_snippets/250_dependency_injection_bad_examples.py
@@ -0,0 +1,31 @@
+class StripePaymentHandler: # do not show in slide
+    def handle_payment():
+        pass
+
+def process_payment(amount: int) -> None:
+    logging.info(f"Order total is ${amount/100:.2f}.")
+    payment_handler = StripePaymentHandler()
+    payment_handler.handle_payment(amount)
+    logging.info("Order completed.")
+
+
+class Com:
+    """Some part of the application which needs to communication
+    via websocket.
+    """
+
+    def __init__(
+        self,
+        host: str = "localhost",
+        port: str = "3000",
+        ...
+    ) -> None:
+        self.host = host
+        self.port = port
+        self.socketio_client = socketio.Client(
+            reconnection=True,
+            reconnection_attempts=10,
+            reconnection_delay=1,
+            reconnection_delay_max=60,
+        )
+
