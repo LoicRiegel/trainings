305101cd66b2086b9c7e6e90134754c3eb199d54
Loïc Riegel <96702577+LoicRiegel@users.noreply.github.com>
2023-06-14 23:52:44 +0200

    improve dep inj inv powerswitch

--- sw-design/presentation_snippets/Write maintainable code/5_dependency_injection_and_inversion/257_dependency_injection_inversion_powerswitch.py
+++ sw-design/presentation_snippets/Write maintainable code/5_dependency_injection_and_inversion/257_dependency_injection_inversion_powerswitch.py
@@ -1,4 +1,39 @@
+# FRANCOIS: to use in code quizz
+
+
 # BEFORE
+
+
+class LightBulb:
+    def turn_on(self):
+        print("LightBulb: turned on...")
+
+    def turn_off(self):
+        print("LightBulb: turned off...")
+
+
+class ElectricPowerSwitch:
+    """Switch that can turn on or off connected devices."""
+    def __init__(self):
+        self.light_bulb = LightBulb()
+        self.on = False
+
+    def press(self):
+        if self.on:
+            self.light_bulb.turn_off()
+            self.on = False
+        else:
+            self.light_bulb.turn_on()
+            self.on = True
+
+def main():
+    electrical_power_switch = ElectricPowerSwitch()
+    electrical_power_switch.press()
+    electrical_power_switch.press()
+
+
+# AFTER DEPENDENCY INJECTION
+
 
 class LightBulb:
     def turn_on(self):
@@ -22,8 +57,60 @@ class ElectricPowerSwitch:
             self.light_bulb.turn_on()
             self.on = True
 
+
 def main():
-    l = LightBulb()
-    switch = ElectricPowerSwitch(l)
-    switch.press()
-    switch.press()
+    light_bulb = LightBulb()
+    electrical_power_switch = ElectricPowerSwitch(light_bulb)
+    electrical_power_switch.press()
+    electrical_power_switch.press()
+
+
+# AFTER DEP INJECTION
+
+from abc import ABC, abstractmethod
+
+class ConnectedDevice(ABC):
+    @abstractmethod
+    def turn_on(self):
+        """Turn on the connected device."""
+
+    @abstractmethod
+    def turn_off(self):
+        """Turn off the connected device."""
+
+class LightBulb(ConnectedDevice):
+    def turn_on(self):
+        print("LightBulb: turned on...")
+
+    def turn_off(self):
+        print("LightBulb: turned off...")
+
+
+class SmartPlug(ConnectedDevice):
+    def turn_on(self):
+        print("SmartPlug: turned on...")
+
+    def turn_off(self):
+        print("SmartPlug: turned off...")
+
+
+class ElectricPowerSwitch:
+    """Switch that can turn on or off connected devices."""
+
+    def __init__(self, connected_device: ConnectedDevice):
+        self.connected_device = connected_device
+        self.on = False
+
+    def press(self):
+        if self.on:
+            self.connected_device.turn_off()
+            self.on = False
+        else:
+            self.connected_device.turn_on()
+            self.on = True
+
+def main():
+    light_bulb = LightBulb()
+    electrical_power_switch = ElectricPowerSwitch(light_bulb)
+    electrical_power_switch.press()
+    electrical_power_switch.press()
